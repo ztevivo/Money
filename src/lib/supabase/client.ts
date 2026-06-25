77b1a8a258f42a78249b6e9286151770177114a6
ztevivo <104692280+ztevivo@users.noreply.github.com>
2026-06-25 11:04:17 -0300

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.NEXT_https://supabase.co.bttwtezmcyfqyfhshrtq;
+const supabaseAnonKey = process.env.NEXT_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0dHd0ZXptY3lmcXlmaHNocnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNTk3NjYsImV4cCI6MjA5NzkzNTc2Nn0.PBzv3PeG91N1ccXJD94EC_wqrCeHP88xp8_PYUJAAiw;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
